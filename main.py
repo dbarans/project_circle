f66b6b5aed5df2cfdf6ebc30b3d6cf6ffab2c02c
Dominik Baranski <baranskidominik109@gmail.com>
2022-04-27 18:05:42 +0200

    koniec zadanie pierwszego

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
-import math
 from math import pi
 
+
 class Circle:
-    def __init__(self, radius):
-        self.radius = radius
-        self.diameter = 2*radius
-        self.area = pi*radius**2
+    def __init__(self, radius=1):
+        self._radius = radius
+        self._diameter = 2 * self._radius
+        self._area = pi * self._radius ** 2
+
+    def __repr__(self):
+        return f"Circle ({self._radius})"
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value > 0:
+            self._radius = value
+            self._diameter = 2 * value
+            self._area = pi * self._radius ** 2
+        else:
+            raise ValueError("Radius cannot be negative")
+
+    @property
+    def area(self):
+        return self._area
+
+    @area.setter
+    def area(self, value):
+        if value > 0:
+            self._area = value
+            self._radius = (self._area / pi) ** (1 / 2)
+            self._diameter = self.radius * 2
+        else:
+            raise ValueError("Area cannot be negative")
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, value):
+        if value > 0:
+            self._diameter = value
+            self._radius = value / 2
+            self._area = pi * self._radius ** 2
+        else:
+            raise ValueError("Diameter cannot be negative")
